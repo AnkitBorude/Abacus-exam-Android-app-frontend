a65f89b73fc4150e0322fbc22d194097e7ab7737
Ankit Borude <ankitborude250@gmail.com>
2024-10-14 19:04:32 +0530

    Added admin fetching exams

--- app/src/main/java/com/example/abacusapplication/AdminMainActivity.java
+++ app/src/main/java/com/example/abacusapplication/AdminMainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.example.abacusapplication.ui.AdminAllExam;
+
 
 public class AdminMainActivity extends AppCompatActivity {
 
@@ -14,7 +16,7 @@ public class AdminMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_main);
         CardView profileCard=findViewById(R.id.profileCard);
         CardView createExamcard=findViewById(R.id.create_exam);
-
+        CardView viewExam=findViewById(R.id.adminViewexam);
         profileCard.setOnClickListener(view->{
             Intent intent=new Intent(getBaseContext(),AdminProfile.class);
             startActivity(intent);
@@ -22,6 +24,11 @@ public class AdminMainActivity extends AppCompatActivity {
 
         createExamcard.setOnClickListener(view -> {
             Intent intent=new Intent(getBaseContext(),AdminCreateExam.class);
+            startActivity(intent);
+        });
+
+        viewExam.setOnClickListener(view->{
+            Intent intent=new Intent(getBaseContext(), AdminAllExam.class);
             startActivity(intent);
         });
     }
